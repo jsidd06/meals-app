eb32d123f4bd205d9398d9c9fcd5e2bcc032adf0
Siddharth Jain <jsidd06@gmail.com>
2023-11-20 06:08:57 +0530

    feat: drawer navigation set

--- App.js
+++ App.js
@@ -2,10 +2,34 @@ import { StatusBar } from "expo-status-bar";
 import CategoriesScreen from "./src/screens/CategoriesScreen";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 import MealsOverViewScreen from "./src/screens/MealsOverViewScreen";
 import MealDetailsScreen from "./src/screens/MealDetails";
+import FavoriteScreen from "./src/screens/FavoriteScreen";
 
 const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function DrawerNavigator() {
+  return (
+    <Drawer.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: "#351401" },
+        headerTintColor: "#fff",
+        sceneContainerStyle: { backgroundColor: "#3f2f25" },
+      }}
+    >
+      <Drawer.Screen
+        name="Categories"
+        options={{
+          title: "All Categories",
+        }}
+        component={CategoriesScreen}
+      />
+      <Drawer.Screen component={FavoriteScreen} name="FavoriteScreen" />
+    </Drawer.Navigator>
+  );
+}
 
 export default function App() {
   return (
@@ -21,10 +45,10 @@ export default function App() {
         >
           <Stack.Screen
             options={{
-              title: "All Categories",
+              headerShown: false,
             }}
-            name="Categories"
-            component={CategoriesScreen}
+            name="Drawer"
+            component={DrawerNavigator}
           />
           <Stack.Screen name="MealsOverView" component={MealsOverViewScreen} />
           <Stack.Screen name="MealDetails" component={MealDetailsScreen} />
